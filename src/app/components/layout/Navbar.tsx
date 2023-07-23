1cfff44ac3f3f4affad482114f515d6b90fe7bad
AbdushukurRasulov <abdushukurrasulov1994@gmail.com>
2023-07-23 07:07:31 +0500

    refactor/ hero v2

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -20,12 +20,61 @@ const links: NavLink[] = [
   { id: 4, label: "Pricing", path: "/" },
 ];
 
-const buttons: NavLink[] = [
-  { id: 1, label: "Contact", path: "/go" },
-  { id: 2, label: "Sign In", path: "/go" },
-];
-
 const Navbar: React.FC = () => {
+  const [scrolled, setScrolled] = useState<Boolean>(false);
+  return (
+    <header className={`fixed top-0 left-0 w-full z-50`}>
+      <nav className='max-w-base mx-auto flex items-center justify-between px-5 py-1 lg:py-5'>
+        <Image
+          width={194}
+          height={52}
+          className='mix-blend-darken'
+          src='/assets/images/logo.png'
+          alt='Logo'
+        />
+        <div className='hidden lg:flex space-x-16 xl:space-x-20 items-center'>
+          <div className='flex items-center space-x-12'>
+            {links.map((link) => (
+              <Link
+                className={`transition-colors duration-200 ${
+                  scrolled ? "lg:hover:text-brand-primary" : "text-white"
+                }`}
+                key={link.id}
+                href='/'>
+                {link.label}
+              </Link>
+            ))}
+          </div>
+          <div className='flex items-center ml-auto space-x-3.5'>
+            <LinkButton href='/' className='bg-white/20 text-white'>
+              Contact
+            </LinkButton>
+            <LinkButton href='/' className='bg-white/20 text-white'>
+              Sign In
+            </LinkButton>
+          </div>
+        </div>
+        {/* <button className='lg:hidden' onClick={handleOpen}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-7 h-7'>
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+            />
+          </svg>
+        </button> */}
+      </nav>
+    </header>
+  );
+};
+
+const OldHeader = () => {
   const [scrolled, setScrolled] = useState<Boolean>(false);
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
@@ -51,10 +100,9 @@ const Navbar: React.FC = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-
   return (
     <header
-      className={`fixed top-0 z-50 inset-x-0 transition-all duration-200 ${
+      className={`bg-brand-primary fixed top-0 z-50 inset-x-0 transition-all duration-200 ${
         scrolled ? "bg-white text-brand-gray-primary shadow" : "text-white"
       }`}>
       {isOpen && <MobileMenu onClose={handleClose} />}
@@ -79,47 +127,6 @@ const Navbar: React.FC = () => {
                 href={link.path}>
                 {link.label}
               </Link>
-            ))}
-          </div>
-          <div className='flex items-center ml-auto space-x-3.5'>
-            {buttons.map((button) => (
-              <LinkButton
-                href={button.path}
-                key={button.id}
-                className={`group gap-x-4 font-medium items-center py-2.5 px-7 ${
-                  scrolled
-                    ? "bg-brand-primary lg:hover:bg-black text-white"
-                    : "bg-white/20 lg:hover:bg-white lg:hover:text-black"
-                }`}>
-                {button.label}
-                <span className='group-hover:translate-x-2 transition-transform duration-200'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='18'
-                    height='17'
-                    viewBox='0 0 18 17'
-                    fill='none'
-                    stroke='currentColor'
-                    className={scrolled ? "lg:hover:fill-black" : ""}>
-                    <g clip-path='url(#clip0_128_18)'>
-                      <path
-                        d='M16.678 7.71511L13.3368 4.3739L12.836 4.87469L16.107 8.14578H0.00238037V8.85411H16.1056L12.8353 12.1245L13.336 12.6253L16.678 9.28336C17.11 8.85128 17.11 8.14719 16.678 7.71511Z'
-                        fill='white'
-                      />
-                    </g>
-                    <defs>
-                      <clipPath id='clip0_128_18'>
-                        <rect
-                          width='17'
-                          height='17'
-                          fill='white'
-                          transform='translate(0.00238037)'
-                        />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                </span>
-              </LinkButton>
             ))}
           </div>
         </div>
--- src/app/components/sections/Hero.tsx
+++ src/app/components/sections/Hero.tsx
@@ -3,42 +3,53 @@ import LinkButton from "../base/LinkButton";
 
 const Hero = () => {
   return (
-    <section className='relative 3xl:max-w-8xl mx-auto px-9 pb-10 md:pb-24 xl:pb-32 overflow-hidden'>
-      <div className='max-w-base mx-auto md:flex 2xl:!px-9'>
-        <div className='max-w-xs md:max-w-md xl:max-w-2xl pt-16'>
-          <h1 className='text-3xl md:text-4xl xl:text-6xl font-bold md:pr-16 xl:pr-10'>
-            We’re Here <span className='text-white'>To Help</span> You Build
-            Your Employee <br className='md:hidden' /> Scheduling.
-          </h1>
-          <p className='text-body-sm lg:text-xl text-black w-32 md:w-auto max-w-xs lg:max-w-md mt-7 xl:mt-10'>
-            Build shifts easily and quickly, saving mistakes{" "}
-            <br className='hidden lg:block' /> and a lot of work time.
-          </p>
-          <div className='md:flex items-center gap-4 mt-10 xl:mt-14'>
-            <LinkButton
-              href='/'
-              base
-              className='!flex !justify-between bg-brand-primary text-white py-5'>
-              Start Free Trial
-            </LinkButton>
-            <LinkButton
-              href='/'
-              base
-              className='!flex !justify-between bg-black hover:bg-brand-primary text-white py-5 mt-3 md:mt-0'>
-              Contact Us
-            </LinkButton>
+    <section className='relative py-10 md:py-24 xl:py-32 overflow-hidden'>
+      <div className='relative 3xl:max-w-8xl mx-auto z-10'>
+        <div className='max-w-base mx-auto md:flex px-9'>
+          <div className='max-w-xs md:max-w-md xl:max-w-2xl pt-12 md:pt-16'>
+            <h1 className='text-3xl md:text-4xl xl:text-6xl font-bold md:pr-16 xl:pr-10'>
+              We’re Here <span className='text-white'>To Help</span> You Build
+              Your Employee <br className='md:hidden' /> Scheduling.
+            </h1>
+            <p className='text-body-sm lg:text-xl text-black w-32 md:w-auto max-w-xs lg:max-w-md mt-7 xl:mt-10'>
+              Build shifts easily and quickly, saving mistakes{" "}
+              <br className='hidden lg:block' /> and a lot of work time.
+            </p>
+            <div className='md:flex items-center gap-4 mt-10 xl:mt-14'>
+              <LinkButton
+                href='/'
+                base
+                className='!flex !justify-between bg-brand-primary text-white py-5'>
+                Start Free Trial
+              </LinkButton>
+              <LinkButton
+                href='/'
+                base
+                className='!flex !justify-between bg-black hover:bg-brand-primary text-white py-5 mt-3 md:mt-0'>
+                Contact Us
+              </LinkButton>
+            </div>
+          </div>
+          <div className='max-w-xs md:max-w-full absolute top-28 -right-32 md:top-0 md:right-0 md:relative flex-1 w-full h-56 md:h-[32rem] xl:h-[40rem]'>
+            <div className='md:absolute md:top-0 -left-32 lg:left-0 w-full md:w-[52rem] h-full'>
+              <Image
+                className='object-contain'
+                src='/assets/images/hero-img.png'
+                alt='Hero '
+                fill
+              />
+            </div>
           </div>
         </div>
-        <div className='max-w-xs md:max-w-full absolute top-32 -right-32 md:top-0 md:right-0 md:relative flex-1 w-full h-56 md:h-[32rem] xl:h-[40rem]'>
-          <div className='md:absolute md:top-0 -left-32 lg:left-0 w-full md:w-[52rem] h-full'>
-            <Image
-              className='object-contain'
-              src='/assets/images/hero-img.png'
-              alt='Hero '
-              fill
-            />
-          </div>
-        </div>
+      </div>
+
+      <div className='w-3/4 h-[91%] 3xl:w-2/3 absolute -top-56 xl:-top-80 -right-24 md:right-0 2xl:-right-20 rotate-12'>
+        <Image
+          className='object-fill'
+          src='/assets/images/bg-effect.png'
+          fill
+          alt=''
+        />
       </div>
     </section>
   );
